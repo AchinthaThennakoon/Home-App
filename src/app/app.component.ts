4051183b8a3d0e87460b878a49be7da820ef54d5
Achintha Thennnakoon <achinthathennnakoon97@gmail.com>
2023-08-15 00:33:58 +0530

    add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  template: `<h1>Hello world!</h1>`,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
